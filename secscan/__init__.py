855a0596b6dcae53ba2cacec12aef108277f867d
Z3r0Bytek <z3robytek@gmail.com>
2026-06-05 22:49:27 +0200

    Initialize secscan package

---
+++ secscan/__init__.py
@@ -0,0 +1,9 @@
+"""
+Secscan - Security Configuration Validator
+
+A tool to validate security configurations across multiple platforms
+against industry standards and best practices.
+"""
+
+__version__ = "0.1.0"
+__author__ = "z3robytek-lang"
